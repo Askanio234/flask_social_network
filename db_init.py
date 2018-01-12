fe516434e37b727c6355694c303d127746507326
Artem Chernyaev <askanio234@gmail.com>
2018-01-12 23:52:59 +0300

    Fix table creation

--- db_init.py
+++ db_init.py
@@ -3,3 +3,9 @@ import models
 
 if __name__ == '__main__':
     models.initialize()
+    models.User.create_user(
+        username="Bob",
+        email="example@example.com",
+        password="password",
+        admin=True
+    )
